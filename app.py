9e5247f0ca259fff8180936e02fd8a21ab65d8c2
smcgloin <smcgloin@terpmail.umd.edu>
2024-03-04 21:29:59 -0500

    Added route for styled about page

--- app.py
+++ app.py
@@ -15,5 +15,9 @@ def hello():  # put application's code here
 def about():  # put application's code here
     return render_template('about.html')
 
+@app.route('/about-css')
+def aboutcss():  # put application's code here
+    return render_template('about-css.html')
+
 if __name__ == '__main__':
     app.run()
