43ba19dca13fdfa1b36bfc244c5efa432a86f102
smcgloin <smcgloin@terpmail.umd.edu>
2024-03-25 21:55:20 -0400

    Contact and favorite course routes created.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -18,6 +18,22 @@ def about():  # put application's code here
 @app.route('/about-css')
 def aboutcss():  # put application's code here
     return render_template('about-css.html')
+
+@app.route('/favorite-course')
+def favoritecourse():  # put application's code here
+    subject = request.args.get('subject')
+    course_number = request.args.get('course_number')
+
+    print('Subject entered: ' + request.args.get('subject'))
+    print('Course number entered' + request.args.get('course_number'))
+    return render_template('favorite-course.html', subject=subject, course_number=course_number)
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():  # put application's code here
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted = True)
+    else:
+        return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run()
